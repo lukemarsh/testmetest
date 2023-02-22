7463691c32b3c59448c021d7a8de7870954d6957
Luke Marsh <lukemarsh@Lukes-Laptop.local>
2023-02-22 13:38:27 +0000

    get some data and do some things with it

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,28 +2,22 @@
 import { WebClient } from "@slack/web-api";
 
 export default async function handler(req, res) {
-  console.log('REQ', req.data);
-  console.log('BODY', req.body);
-
-  const rawBody = (await buffer(req)).toString()
-  const data = JSON.parse(rawBody);
-
-  console.log('DATA', data);
-
   const token = process.env.SLACK_TOKEN;
-
   const web = new WebClient(token);
 
-  const conversationId = "C04PAEGMYRL";
-
-  const chats = await web.conversations.history({
-    channel: conversationId
-  })
-
-  const response = await web.pins.add({
-    channel: conversationId,
-    timestamp: "1677057296.029579"
+  await web.pins.add({
+    channel: req.body.channel,
+    timestamp: req.body.timestamp
   });
+
+  const userGroup = await web.usergroups.create({
+    name: 'testrota',
+  });
+
+  await web.usergroups.users.update({
+    usergroup: userGroup.usergroup.id,
+    users: 'UEC3MJXRT'
+  })
   
   res.status(200).json({ name: 'John Doe' })
 }
