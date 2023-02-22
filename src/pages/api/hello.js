555933e5217b6f83ab1c9ad2b4bd0ec06649c295
Luke Marsh <lukemarsh@Lukes-Laptop.local>
2023-02-22 11:38:00 +0000

    first commit

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { WebClient } from "@slack/web-api";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const token = "xoxp-2590481275-4717440584342-4837941913541-94136aea5f3900b47a68c0c20198733b";
+
+  const web = new WebClient(token);
+
+  const conversationId = "C04PAEGMYRL";
+
+  const chats = await web.conversations.history({
+    channel: conversationId
+  })
+
+  const response = await web.pins.add({
+    channel: conversationId,
+    timestamp: "1677057296.029579"
+  });
+  
   res.status(200).json({ name: 'John Doe' })
 }
