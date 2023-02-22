d634e080a469e4ac94cb8371b355c8a0b980b918
Luke Marsh <lukemarsh@Lukes-Laptop.local>
2023-02-22 12:41:09 +0000

    use env variable

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,7 +2,7 @@
 import { WebClient } from "@slack/web-api";
 
 export default async function handler(req, res) {
-  const token = "thisismytoken";
+  const token = process.env.SLACK_TOKEN;
 
   const web = new WebClient(token);
 
