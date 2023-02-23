0a666ce82c5dfe1296173a4b4dbfa02cd55e557e
Luke Marsh <lukemarsh@Lukes-Laptop.local>
2023-02-23 09:12:04 +0000

    adding try catch

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -4,29 +4,49 @@ import { WebClient } from "@slack/web-api";
 export default async function handler(req, res) {
   const token = process.env.SLACK_TOKEN;
   const web = new WebClient(token);
+  const { body : { firefighter, channel, timestamp } } = req;
+  const users = [
+    firefighter,
+    "U2D1SE9FV",
+    "UDPUWG5KK",
+    "UBQRDLJ5A",
+    "UE1F0TZK7",
+    "U04M3CYH6A2"
+  ];
+  const userGroupId = "S04QKARLE14";
 
   const pins = await web.pins.list({
-    channel: req.body.channel,
+    channel: channel,
   });
 
   console.log(pins);
 
-  await web.pins.remove({
-    channel: req.body.channel,
-    timestamp: pins.items[0].created
-  });
+  try {
+    await web.pins.remove({
+      channel: channel,
+      timestamp: pins.items[0].message.ts
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 
-  await web.pins.add({
-    channel: req.body.channel,
-    timestamp: req.body.timestamp
-  });
+  try {
+    await web.pins.add({
+      channel: channel,
+      timestamp: timestamp
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 
-  const users = [req.body.firefighter, "U2D1SE9FV", "UDPUWG5KK", "UBQRDLJ5A", "UE1F0TZK7", "U04M3CYH6A2"]
-
-  await web.usergroups.users.update({
-    usergroup: "S04QKARLE14",
-    users: users.join(),
-  })
+  try {
+    await web.usergroups.users.update({
+      usergroup: userGroupId,
+      users: users.join(),
+    })
+  } catch (error) {
+    throw new Error(error);
+  }
   
   res.status(200).json({ success: 'great success' })
 }
