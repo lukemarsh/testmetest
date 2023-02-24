178ea327eb49d95987834948620e5546939bf55f
Luke Marsh <lukemarsh@Lukes-Laptop.local>
2023-02-24 09:22:27 +0000

    dont parse users

--- src/pages/api/updateUserGroup.js
+++ src/pages/api/updateUserGroup.js
@@ -10,20 +10,10 @@ export default async function handler(req, res) {
     return res.status(400).send("Invalid parameters supplied");
   }
 
-//   const users = [
-//     firefighter,
-//     "U2D1SE9FV",
-//     "UDPUWG5KK",
-//     "UBQRDLJ5A",
-//     "UE1F0TZK7",
-//     "U04M3CYH6A2"
-//   ];
-//   const userGroupId = "S04QKARLE14";
-
   try {
     await web.usergroups.users.update({
       usergroup: usergroup,
-      users: JSON.parse(users).join(),
+      users: users,
     })
   } catch (error) {
     return res.status(error.requestResult.statusCode).send(error.message);
